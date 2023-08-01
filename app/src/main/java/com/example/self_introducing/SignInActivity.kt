05197f95a8181022734d631b457785a4b1e4e487
Martin Kwon <mindori9097@gmail.com>
2023-08-01 15:25:08 +0900

    230801

---
+++ app/src/main/java/com/example/self_introducing/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.self_introducing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signinactivity_main)
+    }
+}
