c9dffb152cc5b0d5fef1c9c82a99c6e6905520bd
Martin Kwon <mindori9097@gmail.com>
2023-08-07 14:09:09 +0900

    rdmImg@homeActi

--- app/src/main/java/com/example/self_introducing/HomeActivity.kt
+++ app/src/main/java/com/example/self_introducing/HomeActivity.kt
@@ -3,15 +3,19 @@ package com.example.self_introducing
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homeactivity)
+
         val textView6 = findViewById<TextView>(R.id.textView6)
+
         val getID = intent.getStringExtra("ID")
         textView6.text = "아이디: $getID"
 
@@ -21,5 +25,17 @@ class HomeActivity : AppCompatActivity() {
 
             finish()
         }
+
+        val logo = findViewById<ImageView>(R.id.imageView3)
+
+        val id = when ((1..6).random()){
+            1 -> R.drawable.astronaut
+            2 -> R.drawable.cheerup
+            3 -> R.drawable.codinginthedream
+            4 -> R.drawable.heart
+            else -> R.drawable.image
+        }
+
+        logo.setImageDrawable(ResourcesCompat.getDrawable(resources, id, null))
     }
 }
